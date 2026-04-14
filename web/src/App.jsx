ac57f9fc719bb48d937f85ee806a8edd94eff1f8
Surain Saigal <surainsaigal@gmail.com>
2026-04-14 12:50:27 -0500

    add descriptions

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -18,6 +18,13 @@ const DATES = {
 
 const SONIFICATION_TYPES = { "Single Sine Wave": "buscount", "Chord Progression": "avgspeed" };
 
+const DESCRIPTIONS = {
+    buscount:
+        "This is the simplest example of sonification, where the number of buses on the road is represented by a single sine wave tone. The frequency of the sine wave increases as more buses are on the road, creating a rising pitch that reflects bus traffic volume. In order to truly feel the trends, a 24-hour period has been compressed to just over 2 minutes, where each frame of the video represents 30 seconds of real time (you can see the real time in the top right corner). \n\nThe accompanying animation is a visual representation to complement the sound you are hearing. In it, each bus is shown at it's current location, along with a trail representing the past 2 minutes of location data.",
+    avgspeed:
+        "This is a more complicated and artistic approach to sonification. Here, we have an underlying composition driving the chords, but the tempo, number of notes in the chord voicings, and pitch drift are all controlled by the bus data. \n\nYou can hear how there is a sparse, slow arrangement in the early morning times, when not much activity occurs. However, in the peak times of day, we hear a lush and fast arrangement. We also hear the pitch drift (detunement) of the chords increase during periods of high congestion, controlled by traffic speed.",
+};
+
 const BIN = "30s";
 
 export default function App() {
@@ -70,6 +77,16 @@ export default function App() {
                             CapMetro Sonification Gallery{" "}
                             <em className="text-secondary">by Surain Saigal</em>
                         </h4>
+                        <p className="">
+                            This gallery shows various methods of "sonifying" the data of Austin's
+                            CapMetro bus system. Data Sonification is the process of transforming
+                            raw data into sound to analyze and interpret information through audio.
+                            In this case, I have taken CapMetro GPS data, available freely through
+                            the Texas Open Data Portal, and demonstrated multiple ways to sonify
+                            this data. All visuals and audio have been generated programmatically.
+                            Feel free to adjust the selectors below to explore.
+                        </p>
+                        <hr />
                         <div className="d-flex gap-3 align-items-start">
                             <div className="mb-3 flex-grow-1">
                                 <label htmlFor="date-select" className="form-label fw-medium">
@@ -94,7 +111,7 @@ export default function App() {
                                     htmlFor="sonification-select"
                                     className="form-label fw-medium"
                                 >
-                                    Sonification
+                                    Sonification Type
                                 </label>
                                 <select
                                     id="sonification-select"
@@ -111,6 +128,9 @@ export default function App() {
                             </div>
                         </div>
                         <hr />
+                        <p style={{ whiteSpace: "pre-line" }} className="fs-6">
+                            {DESCRIPTIONS[sonification]}
+                        </p>
                     </div>
                 </div>
             </div>
